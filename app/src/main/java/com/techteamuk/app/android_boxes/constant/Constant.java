24ea42c01fc643e989c0dcf35e60309490374817
jimsawinner <jd364@uni.brighton.ac.uk>
2017-01-17 22:56:24 +0000

    Broadcaster Receivers now Implemented (Week 10)

---
+++ app/src/main/java/com/techteamuk/app/android_boxes/constant/Constant.java
@@ -0,0 +1,10 @@
+package com.techteamuk.app.android_boxes.constant;
+
+/**
+ * Created by jim on 17/01/2017.
+ */
+
+public class Constant {
+    public final static String LOBBY_MSG = "com.techteamuk.app.constant.LOBBY_MSG";
+
+}
--- app/src/main/java/com/techteamuk/app/android_boxes/fragments/PlanetListFragment.java
+++ app/src/main/java/com/techteamuk/app/android_boxes/fragments/PlanetListFragment.java
@@ -1,6 +1,7 @@
 package com.techteamuk.app.android_boxes.fragments;
 
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -8,6 +9,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.techteamuk.app.android_boxes.R;
+import com.techteamuk.app.android_boxes.constant.Constant;
 
 /**
  * Created by jim on 16/01/2017.
@@ -33,5 +35,15 @@ public class PlanetListFragment extends ListFragment {
 
         // Update image fragment
         ((PlanetDetailFragment)getFragmentManager().findFragmentById(R.id.fragment_centre)).setImage(Planet.planetResource[position]);
+
+        // Broadcast message to LOBBY_MSG register listeners
+        broadcastIntent();
+    }
+
+    private void broadcastIntent() {
+        Intent intent = new Intent();
+        intent.setAction(Constant.LOBBY_MSG);
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        getActivity().sendBroadcast(intent);
     }
 }
